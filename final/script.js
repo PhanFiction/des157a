b8eebf11cfb84fda2eb9e0323c008d801dfd5832
Steve <stevewu24@gmail.com>
2023-12-08 21:19:50 -0800

    updated final

--- final/script.js
+++ final/script.js
@@ -13,35 +13,47 @@
   const player1Score = document.getElementById('player1-score');
   const player2Score = document.getElementById('player2-score');
   const actionArea = document.querySelector('#actions');
-  const infoSection = document.querySelector('#info-container');
+  const messageContainer = document.getElementById('message-container');
   const audio = document.getElementById('myAudio');
 
   const gameData = {
     dice: ['images/dice1.png', 'images/dice2.png', 'images/dice3.png', 'images/dice4.png', 'images/dice5.png', 'images/dice6.png'],
-    players: ['player1', 'player2'],
+    players: [
+      {
+        player: 'player1',
+        name: '',
+      },
+      {
+        player: 'player2',
+        name: '',
+      }
+    ],
     score: [0, 0],
     roll1: 0,
     roll2: 0,
     rollSum: 0,
     index: 0,
+    maxScore: 30,
   };
 
   function gameForm(event) {
     event.preventDefault();
 
     const maxScore = document.getElementById('score').value;
+    gameData.maxScore = maxScore;
 
-    console.log('Player 1 Name:', player1.value);
-    console.log('Player 2 Name:', player2.value);
-    console.log('Max Score:', maxScore);
-
-    document.getElementById('player1').innerText = player1.value;
+    player1.innerText = player1Name.value;
+    player2.innerText = player2Name.value;
+    gameData.players[0].name = player1Name.value;
+    gameData.players[1].name = player2Name.value;
     document.getElementById('game-overlay').style.display = 'none';
   }
 
-  function addInfo(text) {
-    document.getElementById('player1').innerText = player1.value;
-    infoSection.innerHTML += `<span>${text}</span>`;
+  function addInfo(text, playerId) {
+    const message = playerId == null ? `<span>${text}</span>`
+      :`<span style="color:blue">${gameData.players[gameData.index]}</span>`;
+
+    messageContainer.innerHTML += message;
   }
 
   function handleScore(player, score) {
@@ -63,7 +75,7 @@
 })
 
 function setUpTurn() {
-  addInfo(`${gameData.players[gameData.index]} turn`);
+  addInfo(`<span style="color:blue">${gameData.players[gameData.index].name}</span> turn`);
   actionArea.innerHTML = '<button id="roll" class="btn">Roll</button>';
   document.getElementById('roll').addEventListener('click', ()=>{
     throwDice();
@@ -75,25 +87,25 @@ function throwDice() {
     gameData.roll1 = Math.floor(Math.random() * 6) + 1;
     gameData.roll2 = Math.floor(Math.random() * 6) + 1;
     game.innerHTML = `<p>${gameData.players[gameData.index]} Roll</p>`;
-    game.innerHTML = `<img src="${gameData.dice[gameData.roll1-1]}">
-        <img src="${gameData.dice[gameData.roll2-1]}">`;
+    game.innerHTML = `<img class="dice" src="${gameData.dice[gameData.roll1-1]}">
+        <img class="dice" src="${gameData.dice[gameData.roll2-1]}">`;
     gameData.rollSum = gameData.roll1 + gameData.roll2;
     audio.play();
     if(gameData.rollSum === 2) {
-        addInfo('Oh snap Snake eyes');
+        addInfo('Oh snap Snake eyes ');
         gameData.score[gameData.index] = 0;
         handleScore(gameData.index, 0);
         gameData.index ? gameData.index = 0 : gameData.index = 1;
         setTimeout(setUpTurn, 2000);
     }else if(gameData.roll1 === 1 || gameData.roll2 === 1){
         gameData.index ? gameData.index = 0 : gameData.index = 1;
-        addInfo(`sorry, your roll was a one, switchting to ${gameData.players[gameData.index]}`)
+        addInfo(`sorry, your roll was a one, switchting to ${gameData.players[gameData.index].name} `)
         setTimeout(setUpTurn, 2000);
     }else{
         const totalScore = gameData.score[gameData.index] + gameData.rollSum;
         gameData.score[gameData.index] = totalScore;
         handleScore(gameData.index, totalScore);
-        actionArea.innerHTML = '<button id="rollagain" class="btn">Roll again</button> or <button id="pass" class="btn">Pass</button>';
+        actionArea.innerHTML = '<button id="rollagain" class="btn">Roll</button> <button id="pass" class="btn">Pass</button>';
         document.getElementById('rollagain').addEventListener('click', ()=>{
           setUpTurn();
         });
@@ -106,18 +118,19 @@ function throwDice() {
 }
 
 function checkWinningCondition() {
-    if(gameData.score[gameData.index] > gameData.gameEnd) {
-      addInfo(`${gameData.players[gameData.index]} wins with ${gameData.score[gameData.index]} points!`);
+    if(gameData.score[gameData.index] > gameData.maxScore) {
+      addInfo(`${gameData.players[gameData.index].name} wins with ${gameData.score[gameData.index]} points!`);
       actionArea.innerHTML = '';
-      document.getElementById('quit').innerHTML = "Start a new game";
+      document.getElementById('quit').innerHTML = "Reset";
     }else{
       showCurrentScore();
     }
 }
 
 function showCurrentScore() {
-    addInfo(`Score for ${gameData.players[0]} with
-    ${gameData.score[0]} and ${gameData.players[1]} with
+  console.log(gameData.players[0].name);
+    addInfo(`Score for ${gameData.players[0].name} with
+    ${gameData.score[0]} and ${gameData.players[1].name} with
     ${gameData.score[1]}`);
 }
 
